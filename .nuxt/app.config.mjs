344405e37b7f18a0a8672be5e44405b337bcd276
emojiiii <1207145565@qq.com>
2023-05-28 11:27:57 +0800

    chore: 移除 .nuxt

--- .nuxt/app.config.mjs
+++
@@ -1,8 +0,0 @@
-
-import { defuFn } from 'D:/project/blog/node_modules/.pnpm/defu@6.1.2/node_modules/defu/dist/defu.mjs'
-
-const inlineConfig = {}
-
-import cfg0 from "D:/project/blog/app.config.ts"
-
-export default /* #__PURE__ */ defuFn(cfg0, inlineConfig)
--- .nuxt/types/layouts.d.ts
+++
@@ -1,7 +0,0 @@
-import { ComputedRef, Ref } from 'vue'
-export type LayoutKey = "default"
-declare module "D:/project/blog/node_modules/.pnpm/nuxt@3.5.1_@types+node@18.15.12/node_modules/nuxt/dist/pages/runtime/composables" {
-  interface PageMeta {
-    layout?: false | LayoutKey | Ref<LayoutKey> | ComputedRef<LayoutKey>
-  }
-}
