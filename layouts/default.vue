98da12281669796083833a68169f3b57a0672ab4
emojiiii <1207145565@qq.com>
2023-10-23 18:45:37 +0800

    feat: init blog

--- layouts/default.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <div class="font-sans text-lg w-full h-full sm:pt-6 sm:pb-10 bg-gray-100">
-        <slot />
-    </div>
-</template>
--- pages/[...slug].vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <NuxtLayout>
-    <div class="flex justify-center ">
-      <ContentDoc class="lg:max-w-4xl p-4 rounded-lg shadow-lg bg-white" />
-    </div>
-  </NuxtLayout>
-</template>
