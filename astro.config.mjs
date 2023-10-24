da958739a899e2eace5396959ddf9b6d947f939f
emojiiii <1207145565@qq.com>
2023-10-24 22:44:45 +0800

    fix: update image path

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://emojiiii.github.io',
-	base: '/blog',
+	site: 'https://blog.kitt.tools',
 	integrations: [mdx(), sitemap()],
 });
